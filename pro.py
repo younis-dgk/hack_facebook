7837d06a7e172fefa78ed5b6e5ab4370fe027c05
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-09 16:15:25 +0500

    Delete pro.py

--- pro.py
+++
@@ -1,9 +0,0 @@
-import os,re,sys,platform
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from Pro import XYZ
-    XYZ()
-elif bit == '32bit':
-    print(f"\033[1;91m Sorry 32Bit Not Supported .... ")
- 
